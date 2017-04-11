00b1af5b7e5979cac95e52e13b0db82648f66ea5
cg1101 <cg1101@gmail.com>
2017-04-11 14:19:03 +1000

    added shell script for command and template 4 to create vpc

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+aws cloudformation create-stack --stack-name My-Stack-`date +%Y%m%sT%H%M%SZ` \
+  --region ap-northeast-1 \
+  --template-body file://./exercise1.json
+
+aws cloudformation create-stack --stack-name My-Stack-`date +%Y%m%sT%H%M%SZ` \
+  --region ap-northeast-1 \
+  --template-body file://./exercise2.json
+
+aws cloudformation create-stack --stack-name My-Stack-`date +%Y%m%sT%H%M%SZ` \
+  --region ap-northeast-1 \
+  --template-body file://./exercise3.json \
+  --parameters ParameterKey=EC2KeyName,ParameterValue=tokyo_key_pair_1
+
+aws cloudformation create-stack --stack-name My-Stack-`date +%Y%m%sT%H%M%SZ` \
+  --region ap-northeast-1 \
+  --template-body file://./exercise4.json \
+  --parameters ParameterKey=VPCIPRange,ParameterValue=10.0.0.0/16 \
+    ParameterKey=Region,ParameterValue=ap-northeast-1
+
